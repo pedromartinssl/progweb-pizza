f33e54dda497fa51f17217a88a87af535107cac9
boyz <pedroviniciosrn@gmail.com>
2026-06-19 13:21:15 +0000

    Integrando Banco de Dados

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template
+import json
+from flask import flash, redirect
+from utils import db
+import os
+from flask_migrate import Migrate
+from models import Usuario
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db_usuario = os.getenv('DB_USERNAME')
+db_senha = os.getenv('DB_PASSWORD')
+db_database = os.getenv('DB_DATABASE')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+
+conexao = f"mysql+pymysql://{db_usuario}:{db_senha}@{db_host}:{db_port}/{db_database}"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
