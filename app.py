b66e312c2ba952db7356b9d1be3118ccd28552ca
boyz <pedroviniciosrn@gmail.com>
2026-06-19 18:27:03 +0000

    Ajustes da integração

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import json
-from flask import flash, redirect
 from utils import db
 import os
 from flask_migrate import Migrate
@@ -10,15 +9,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db_usuario = os.getenv('DB_USERNAME')
 db_senha = os.getenv('DB_PASSWORD')
-db_database = os.getenv('DB_DATABASE')
+db_mydb = os.getenv('DB_DATABASE')
 db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
 
-conexao = f"mysql+pymysql://{db_usuario}:{db_senha}@{db_host}:{db_port}/{db_database}"
+conexao = f"mysql+pymysql://{db_usuario}:{db_senha}@{db_host}:{db_port}/{db_mydb}"
+print(conexao)
 app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -31,13 +29,13 @@ def index():
 def cardapio():
     return render_template('cardapio.html')
 
-@app.route('/avaliacoes')
-def avaliacoes():
-    return render_template('avaliacoes.html')
-
 @app.route('/faleconosco')
 def faleconosco():
     return render_template('faleconosco.html')
+
+@app.route('/avaliacoes')
+def avaliacoes():
+    return render_template('avaliacoes.html')
 
 @app.route('/login')
 def login():
